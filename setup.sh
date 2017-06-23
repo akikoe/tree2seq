54483acdb1f721df32fd9bf238a6389fc84a50a5
Akiko Eriguchi <eriguchi@logos.t.u-tokyo.ac.jp>
2017-06-23 10:38:22 +0900

    fix #3
    Modified setup.sh:
    
    - Revision of the path of N3LP
    - Copy Optimizer.*pp from N3LP

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 # You are in tree2seq/
-git clone git@github.com:hassyGo/N3LP.git
+git clone https://github.com/hassyGo/N3LP.git
 cd N3LP
-cp LSTM.* TreeLSTM.* BlackOut.* SoftMax.* Vocabulary.* ..
+cp LSTM.* TreeLSTM.* BlackOut.* SoftMax.* Vocabulary.* Optimizer.* ..
 cd ..
 mkdir objs
 make
